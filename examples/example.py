fd8c57845ab2710f40cdc609e06cc16c92d7e0fe
AIMotionLab <aimotionlab.sztaki@gmail.com>
2024-01-24 10:55:54 +0100

    reworking packaging

--- examples/example.py
+++ examples/example.py
@@ -1,5 +1,5 @@
 import numpy as np
-from skyc_maker import Trajectory, write_skyc, XYZYaw, select_file, evaluate_pickle
+from skyc_utils.skyc_maker import Trajectory, write_skyc, XYZYaw, select_file, evaluate_pickle
 
 
 def fig8(t, x_start, y_start, x_max, y_max, z, yaw_max=0.0):
